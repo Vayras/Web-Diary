a518b23c36fb37dd59a27645c16d66691f936f08
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-05-02 03:58:07 +0530

    profile separation with user authentication

--- web_diary/diary_entry/forms.py
+++ web_diary/diary_entry/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import ModelForm, widgets
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Entry
 
 class EntryForm(ModelForm):
@@ -14,3 +16,17 @@ class EntryForm(ModelForm):
     class Meta:
         model = Entry
         fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+
+    username = forms.CharField(label="name",widget=forms.TextInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5', 'placeholder':'name'}))
+
+    email = forms.CharField(label="email",widget=forms.TextInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5', 'placeholder':'username@gmail.com'}))
+
+    password1 = forms.CharField(label="password",widget=forms.PasswordInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5', 'placeholder':'••••••••'}))
+
+    password2 = forms.CharField(label="re-password",widget=forms.PasswordInput(attrs={'class':'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5', 'placeholder':'••••••••'}))
+    
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
--- web_diary/diary_entry/views.py
+++ web_diary/diary_entry/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, request
 from .models import *
-from .forms import EntryForm
+from .forms import EntryForm, CreateUserForm
 from .filters import EntryFilter
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
+# we cannot access the home page unless we have logged in otherwise we will be directed back to the login page.
 def home(request): 
 
     entries = Entry.objects.all()
@@ -19,6 +25,7 @@ def home(request):
 
     return render(request, 'diary_entry/home.html', context)
 
+@login_required(login_url='login')
 def entry(request):
     form = EntryForm
 
@@ -33,6 +40,7 @@ def entry(request):
 
     return render(request, 'diary_entry/entry_form.html', context)
 
+@login_required(login_url='login')
 def PreviewEntry(request, pk):
     entry_info = get_object_or_404(Entry, id=pk)
 
@@ -49,14 +57,54 @@ def PreviewEntry(request, pk):
 
     return render(request, 'diary_entry/entry_form.html', context)
 
+@login_required(login_url='login')
 def deleteEntry(request):
 
     context = {}
 
     return render(request, 'diary_entry/delete.html', context)
 
-def login(request):
-    return render(request, 'diary_entry/sign_in.html')
+def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    else:
+        if request.method == "POST":
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            
+            else:
+                messages.info(request, 'Username or password is incorrect.')
+
+        return render(request, 'diary_entry/sign_in.html')
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
 
 def register(request):
-    return render(request, 'diary_entry/registration_page.html')
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    else:
+        form = CreateUserForm()
+
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' +  user)
+                return redirect('login')                                                   
+        
+        context={'form':form}
+
+        return render(request, 'diary_entry/registration_page.html', context)
